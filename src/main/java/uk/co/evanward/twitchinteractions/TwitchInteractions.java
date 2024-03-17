a113cabd21864cff4c549afcc02c9fe7060b4b4c
Evan Ward <Evan29Ward@gmail.com>
2024-03-17 00:15:42 +0000

    Added logger

--- src/main/java/uk/co/evanward/twitchinteractions/TwitchInteractions.java
+++ src/main/java/uk/co/evanward/twitchinteractions/TwitchInteractions.java
@@ -1,16 +1,21 @@
 package uk.co.evanward.twitchinteractions;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.co.evanward.twitchinteractions.twitch.server.websocket.SocketClient;
 
 import java.net.URI;
 
 public class TwitchInteractions implements ModInitializer
 {
+    public static final Logger logger = LoggerFactory.getLogger(TwitchInteractions.class);
     public static SocketClient socketClient;
 
     @Override
     public void onInitialize() {
+        logger.info("Initialising Mod");
+
         socketClient = new SocketClient(URI.create("ws://localhost:8000/ws"));
     }
 }
