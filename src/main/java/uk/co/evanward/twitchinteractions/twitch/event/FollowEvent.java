21fd207d6ea2e05febcca7a957aadb34c69c5b69
Evan Ward <Evan29Ward@gmail.com>
2024-03-18 22:41:41 +0000

    Implemented `FollowEvent`

--- src/main/java/uk/co/evanward/twitchinteractions/twitch/event/FollowEvent.java
+++ src/main/java/uk/co/evanward/twitchinteractions/twitch/event/FollowEvent.java
@@ -1,7 +1,14 @@
 package uk.co.evanward.twitchinteractions.twitch.event;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.passive.AllayEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.json.JSONObject;
 import uk.co.evanward.twitchinteractions.config.ModConfig;
+import uk.co.evanward.twitchinteractions.helpers.AnnouncementHelper;
+import uk.co.evanward.twitchinteractions.helpers.ServerHelper;
 
 public class FollowEvent implements TwitchEvent.TwitchEventInterface
 {
@@ -39,6 +46,18 @@ public class FollowEvent implements TwitchEvent.TwitchEventInterface
     @Override
     public void trigger(JSONObject payload)
     {
+        JSONObject event = payload.getJSONObject("event");
+        String follower = event.getString("user_name");
 
+        ServerPlayerEntity player = ServerHelper.getConnectedPlayer();
+        AnnouncementHelper.playAnnouncement(follower, "Just Followed!");
+
+        AllayEntity allay = new AllayEntity(EntityType.ALLAY, player.getWorld());
+        allay.setPosition(player.getPos());
+        allay.setCustomName(Text.literal(follower));
+        allay.setCustomNameVisible(true);
+        allay.getBrain().remember(MemoryModuleType.LIKED_PLAYER, player.getUuid());
+
+        player.getServerWorld().spawnEntity(allay);
     }
 }
