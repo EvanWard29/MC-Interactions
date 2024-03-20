31bda0373995a7b10dc5ccb2db0727c8956c6482
Evan Ward <Evan29Ward@gmail.com>
2024-03-20 20:26:53 +0000

    Gift sub spawn egg will now drop on the floor if player's inventory is full

--- src/main/java/uk/co/evanward/twitchinteractions/twitch/event/SubscribeEvent.java
+++ src/main/java/uk/co/evanward/twitchinteractions/twitch/event/SubscribeEvent.java
@@ -1,9 +1,13 @@
 package uk.co.evanward.twitchinteractions.twitch.event;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import org.json.JSONObject;
 import uk.co.evanward.twitchinteractions.config.ModConfig;
 import uk.co.evanward.twitchinteractions.helpers.ServerHelper;
@@ -44,7 +48,7 @@ public class SubscribeEvent implements TwitchEvent.TwitchEventInterface
         JSONObject event = payload.getJSONObject("event");
 
         if (event.has("is_gift") && event.getBoolean("is_gift")) {
-            // Gift Sub
+            // <editor-fold desc="Gift Sub">
             SpawnEggItem spawnEggItem = SpawnEggItem.forEntity(EntityType.TROPICAL_FISH);
             ItemStack eggItem = spawnEggItem.asItem().getDefaultStack();
             eggItem.setCount(1);
@@ -61,8 +65,25 @@ public class SubscribeEvent implements TwitchEvent.TwitchEventInterface
             nbt.put("display", itemName);
 
             eggItem.setNbt(nbt);
+            eggItem.setCount(1);
 
-            ServerHelper.getConnectedPlayer().giveItemStack(eggItem);
+            ServerPlayerEntity player = ServerHelper.getConnectedPlayer();
+
+            // Attempt to insert egg into player's inventory
+            boolean inserted = player.getInventory().insertStack(eggItem);
+
+            // Drop the egg on the floor if the player's inventory is full
+            ItemEntity itemEntity;
+            if (!inserted) {
+                itemEntity = player.dropItem(eggItem, false);
+                itemEntity.resetPickupDelay();
+                itemEntity.setOwner(player.getUuid());
+            }
+
+            // Play item pickup sound
+            player.getWorld().playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2f, ((player.getRandom().nextFloat() - player.getRandom().nextFloat()) * 0.7f + 1.0f) * 2.0f);
+            player.currentScreenHandler.sendContentUpdates();
+            // </editor-fold>
         }
     }
 }
