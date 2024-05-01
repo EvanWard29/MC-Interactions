45d10f251ada8fb3c01484290a1d8967600057da
Evan Ward <Evan29Ward@gmail.com>
2024-05-01 22:48:15 +0100

    Mansion entities are now replaced

---
+++ src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/spawns/PieceMixin.java
@@ -0,0 +1,31 @@
+package uk.co.evanward.twitchinteractions.mixins.worldchanges.spawns;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.registry.Registries;
+import net.minecraft.structure.WoodlandMansionGenerator;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import uk.co.evanward.twitchinteractions.TwitchInteractions;
+
+@Mixin(WoodlandMansionGenerator.Piece.class)
+public class PieceMixin
+{
+    @ModifyArg(method = "handleMetadata", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ServerWorldAccess;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V"))
+    private Entity replaceMansionEntity(Entity entity)
+    {
+        if (TwitchInteractions.worldChanges.REPLACE_MOB_SPAWN.contains(entity.getType().toString())) {
+            MobEntity replacement = (MobEntity) Registries.ENTITY_TYPE.get(Identifier.tryParse(TwitchInteractions.worldChanges.REPLACE_MOB_SPAWN.getString(entity.getType().toString())))
+                .create(entity.getWorld());
+
+            replacement.setPosition(entity.getPos());
+            replacement.setPersistent();
+
+            entity = replacement;
+        }
+
+        return entity;
+    }
+}
