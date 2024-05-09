5040a2fe40c6d6fd6422388701f06664f7029c21
Evan Ward <Evan29Ward@gmail.com>
2024-05-09 20:45:19 +0100

    Teleport Home will now teleport to bed, or world spawn if not set

--- src/main/java/uk/co/evanward/twitchinteractions/twitch/event/channelpoints/redemptions/TeleportHomeRedemption.java
+++ src/main/java/uk/co/evanward/twitchinteractions/twitch/event/channelpoints/redemptions/TeleportHomeRedemption.java
@@ -1,15 +1,18 @@
 package uk.co.evanward.twitchinteractions.twitch.event.channelpoints.redemptions;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.json.JSONObject;
 import uk.co.evanward.twitchinteractions.helpers.AnnouncementHelper;
 import uk.co.evanward.twitchinteractions.helpers.ServerHelper;
 import uk.co.evanward.twitchinteractions.twitch.event.channelpoints.ChannelPoint;
+
+import java.util.Optional;
 
 public class TeleportHomeRedemption implements ChannelPoint.ChannelPointInterface
 {
@@ -24,14 +27,26 @@ public class TeleportHomeRedemption implements ChannelPoint.ChannelPointInterfac
         // Get the connected player
         ServerPlayerEntity player = ServerHelper.getConnectedPlayer();
 
-        // Get the Overworld of the server
-        ServerWorld overworld = ServerHelper.getServer().getWorld(World.OVERWORLD);
+        // Get the player's spawn
+        Optional<Vec3d> spawnPoint = PlayerEntity.findRespawnPosition(
+            ServerHelper.getServer().getWorld(World.OVERWORLD),
+            player.getSpawnPointPosition(),
+            player.getSpawnAngle(),
+            false,
+            false
+        );
 
-        // Get the coords of the Overworld spawn
-        BlockPos spawn = overworld.getSpawnPos();
+        // Spawn the player at their spawn point, or the world spawn if not set
+        Vec3d spawn;
+        if (spawnPoint.isPresent()) {
+            spawn = spawnPoint.get();
+        } else {
+            BlockPos blockPos = ServerHelper.getServer().getWorld(World.OVERWORLD).getSpawnPos();
+            spawn = new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+        }
 
         // Teleport the player to the Overworld spawn
-        player.teleport(overworld, spawn.getX(), spawn.getY(), spawn.getZ(), player.getYaw(), player.getPitch());
+        player.teleport(ServerHelper.getServer().getWorld(World.OVERWORLD), spawn.getX(), spawn.getY(), spawn.getZ(), player.getYaw(), player.getPitch());
 
         // Play the announcement sound at the Overworld spawn
         player.playSoundToPlayer(SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, 1f, 0.5f);
