e2953ecae2b14cb50a120f055e3e91663c5298ce
Evan Ward <Evan29Ward@gmail.com>
2024-05-09 20:09:22 +0100

    Fixed day length modifier

--- src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/daylength/ClientWorldMixin.java
+++ src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/daylength/ClientWorldMixin.java
@@ -1,7 +1,10 @@
 package uk.co.evanward.twitchinteractions.mixins.worldchanges.daylength;
 
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import uk.co.evanward.twitchinteractions.TwitchInteractions;
@@ -9,9 +12,23 @@ import uk.co.evanward.twitchinteractions.TwitchInteractions;
 @Mixin(ClientWorld.class)
 public abstract class ClientWorldMixin
 {
+    @Shadow public abstract ClientWorld.Properties getLevelProperties();
+
     @ModifyArg(method = "tickTime", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;setTimeOfDay(J)V"))
-    public long getDayTime(long timeOfDay)
+    public long setTimeOfDay(long timeOfDay)
     {
-        return ((ClientWorld)(Object)this).getLevelProperties().getTimeOfDay() + (long) 24000 / TwitchInteractions.worldChanges.DAY_LENGTH;
+        return this.getLevelProperties().getTimeOfDay() + delta();
+    }
+
+    private int delta()
+    {
+        float f = 24000F / (float) TwitchInteractions.worldChanges.DAY_LENGTH;
+        int i = MathHelper.floor(f);
+
+        if (Random.create().nextFloat() < MathHelper.fractionalPart(f)) {
+            i++;
+        }
+
+        return i;
     }
 }
--- src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/daylength/ServerWorldMixin.java
+++ src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/daylength/ServerWorldMixin.java
@@ -1,6 +1,8 @@
 package uk.co.evanward.twitchinteractions.mixins.worldchanges.daylength;
 
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -12,6 +14,18 @@ public abstract class ServerWorldMixin
     @ModifyArg(method = "tickTime", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V"))
     public long getDayTime(long timeOfDay)
     {
-        return ((ServerWorld)(Object)this).getLevelProperties().getTimeOfDay() + (long)  24000 / TwitchInteractions.worldChanges.DAY_LENGTH;
+        return ((ServerWorld)(Object)this).getLevelProperties().getTimeOfDay() + delta();
+    }
+
+    private int delta()
+    {
+        float f = 24000F / (float) TwitchInteractions.worldChanges.DAY_LENGTH;
+        int i = MathHelper.floor(f);
+
+        if (Random.create().nextFloat() < MathHelper.fractionalPart(f)) {
+            i++;
+        }
+
+        return i;
     }
 }
