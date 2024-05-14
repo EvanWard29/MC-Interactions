3c40a9dd0795ef7a8cbaa742ae76c9eb9d16fe9a
Evan Ward <Evan29Ward@gmail.com>
2024-05-14 18:35:40 +0100

    Fixed creeper mixin not overriding `initialize` on main clients

--- src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/CreeperEntityMixin.java
+++ src/main/java/uk/co/evanward/twitchinteractions/mixins/worldchanges/CreeperEntityMixin.java
@@ -1,28 +1,35 @@
 package uk.co.evanward.twitchinteractions.mixins.worldchanges;
 
 import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import uk.co.evanward.twitchinteractions.TwitchInteractions;
 
 @Mixin(CreeperEntity.class)
-public abstract class CreeperEntityMixin
+public abstract class CreeperEntityMixin extends HostileEntity
 {
     @Shadow @Final private static TrackedData<Boolean> CHARGED;
 
-    @Unique
+    protected CreeperEntityMixin(EntityType<? extends HostileEntity> entityType, World world)
+    {
+        super(entityType, world);
+    }
+
+    @Override
     public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData)
     {
         if (TwitchInteractions.worldChanges.CHARGED_CREEPERS) {
-            ((CreeperEntity)(Object)this).getDataTracker().set(CHARGED, true);
+            this.getDataTracker().set(CHARGED, true);
         }
 
         return entityData;
